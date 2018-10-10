6d4737a8f369e3d95c9a839b93a219b48c176277
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2018-10-11 01:01:30 +0300

    implement (shit) order creation, needs (a lot of) refactoring

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.set('view engine', 'ejs');
 // Middleware setuo
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session.middleware);
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,72 @@ router.get('/', function(req, res, next) {
 
 // Get method for /user/create_order
 router.get('/create_order', function(req, res, next) {
-  res.render('./user/create_order', { authorized: req.session.isActive });
+  // Lookup for the user
+  req.app.db.model('User').findOne({where: {email: req.session.user}})
+    .then((user) => {
+      // Lookup for user's personal info
+      req.app.db.model('Person').findById(user.person_id).then((person) => {
+        // If user has address
+        if (person.address_id) {
+          // Lookup for address and render page with all the data
+          req.app.db.model('Address').findById(person.address_id).then((address) => {
+            res.render('./user/create_order', {
+              authorized: req.session.isActive,
+              person,
+              address
+            });
+          });
+        } else {
+          // If user does not have address, render page without it
+          let address = {};
+          res.render('./user/create_order', {
+            authorized: req.session.isActive,
+            person,
+            address
+          });
+        }
+      });
+    });
+});
+
+// Post method for /user/create_order
+router.post('/create_order', function(req, res, next) {
+  console.log(req.body.delivery);
+  // Lookup for the user
+  req.app.db.model('User').findOne({where: {email: req.session.user}})
+    .then((user) => {
+      // Lookup for user's personal info
+      req.app.db.model('Person').findById(user.person_id).then((person) => {
+        // If user has address
+        req.app.db.model('Address').create(req.body.sender.address).then((send_addr) => {
+          if (!person.address_id) {
+            person.update({address_id: send_addr.id});
+          }
+          req.app.db.model('Address').create(req.body.recipient.address).then((rec_addr) => {
+            req.app.db.model('Person').create({...req.body.sender.name, phone: req.body.sender.phone}).then((sender) => {
+              req.app.db.model('Person').create({...req.body.recipient.name, phone: req.body.recipient.phone}).then((recipient) => {
+
+                req.app.db.model('Order').create({
+                  sender_id: sender.id,
+                  receiver_id: recipient.id,
+                  sender_addr_id: send_addr.id,
+                  receiver_addr_id: rec_addr.id,
+                  pkg_height: req.body.parcel.height,
+                  pkg_wigth: req.body.parcel.width,
+                  pkg_length: req.body.parcel.length,
+                  pkg_weight: req.body.parcel.weight,
+                  express: req.body.delivery.type == 'express',
+                  door_to_door: req.body.delivery.pickup == 'building'
+                }).then(() => {
+                  res.redirect('/user/create_order');
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+
 });
 
 // Get method for /user/edit_account
